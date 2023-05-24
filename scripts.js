094f95f3de6549604c13cd2b86908f6d6b26c097
Patrick Antonio de Oliveira <patrickoli99@gmail.com>
2023-05-24 16:04:18 -0300

    Atualização nos arquivos

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
 const header = document.querySelector(".header");
+document.getElementById("radio1").checked = true;
+let count = 1;
 
 hamburguer.addEventListener("click", () => {
    hamburguer.classList.toggle('active');
    navMenu.classList.toggle('active');
    header.classList.toggle('active');
-})
+});
+setInterval(function () {
+   nextImage();
+}, 5000)
+function nextImage() {
+   count++
+   if (count > 3) {
+      count = 1;
+   }
+   document.getElementById("radio" + count).checked = true;
+}
