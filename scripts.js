e1dfdc6108f163124f9c0feb7e80715956a6aa7a
Patrick Antonio de Oliveira <patrickoli99@gmail.com>
2023-05-30 11:13:11 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
-const header = document.querySelector(".header");
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav-menu');
+const header = document.querySelector('.header');
 let count = 1;
 
 hamburguer.addEventListener("click", () => {
-   hamburguer.classList.toggle('active');
-   navMenu.classList.toggle('active');
-   header.classList.toggle('active');
+   hamburguer.classList.toggle("active");
+   navMenu.classList.toggle("active");
+   header.classList.toggle("active");
 });
+
 setInterval(function () {
    nextImage();
 }, 5000)
 function nextImage() {
-   count++;
+   count++
    if (count > 3) {
       count = 1;
    }
