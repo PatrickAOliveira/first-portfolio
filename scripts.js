fd2e1f49dc42e2a74cbc4d764492babbdbcdf137
Patrick Antonio de Oliveira <patrickoli99@gmail.com>
2023-05-30 15:05:24 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,21 +1,10 @@
 const hamburguer = document.querySelector('.hamburguer');
 const navMenu = document.querySelector('.nav-menu');
 const header = document.querySelector('.header');
-let count = 1;
+
 
 hamburguer.addEventListener("click", () => {
    hamburguer.classList.toggle("active");
    navMenu.classList.toggle("active");
    header.classList.toggle("active");
 });
-
-setInterval(function () {
-   nextImage();
-}, 5000)
-function nextImage() {
-   count++
-   if (count > 3) {
-      count = 1;
-   }
-   document.getElementById("radio" + count).checked = true;
-}
